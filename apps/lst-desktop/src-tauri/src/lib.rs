8eb892bbe3c0d47e6efcf5a2fc6f2e3fca46fd7f
Tommy Falkowski <tommy.falkowski@gmail.com>
2025-06-19 11:20:38 +0200

    feat: working tauri lst-desktop scaffold (no functionality yet)

--- apps/lst-desktop/src-tauri/src/lib.rs
+++ apps/lst-desktop/src-tauri/src/lib.rs
@@ -1,8 +1,8 @@
 use anyhow::Result;
-use lst_cli::storage::list_lists;
-use lst_cli::storage::list_notes;
+use lst_cli::storage::{list_lists, list_notes};
 use serde::{Deserialize, Serialize};
 use specta_typescript::Typescript;
+use tauri::tray::TrayIconBuilder;
 use tauri::Manager;
 use tauri_specta::{collect_commands, Builder};
 
@@ -13,16 +13,16 @@ fn get_lists() -> Result<Vec<String>, String> {
 }
 
 #[tauri::command]
-fn get_notes() -> Vec<std::string::String> {
-    let notes = list_notes();
-    notes.unwrap()
+#[specta::specta]
+fn get_notes() -> Result<Vec<String>, String> {
+    list_notes().map_err(|e| e.to_string())
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     let builder = Builder::<tauri::Wry>::new()
         // Then register them (separated by a comma)
-        .commands(collect_commands![get_lists,]);
+        .commands(collect_commands![get_lists, get_notes]);
 
     #[cfg(debug_assertions)] // <- Only export on non-release builds
     builder
@@ -32,16 +32,17 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .setup(|app| {
+            let tray = TrayIconBuilder::new().build(app)?;
             let window = app.get_webview_window("main").unwrap();
 
-            #[cfg(target_os = "macos")]
-            window_vibrancy::apply_vibrancy(
-                &window,
-                window_vibrancy::NSVisualEffectMaterial::HudWindow,
-                None,
-                Some(12.0),
-            )
-            .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+            // #[cfg(target_os = "macos")]
+            // window_vibrancy::apply_vibrancy(
+            //     &window,
+            //     window_vibrancy::NSVisualEffectMaterial::HudWindow,
+            //     None,
+            //     Some(5.0),
+            // )
+            // .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
 
             #[cfg(target_os = "windows")]
             window_vibrancy::apply_blur(&window, Some((18, 18, 18, 125)))
